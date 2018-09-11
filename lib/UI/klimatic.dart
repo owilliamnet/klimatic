db30b0230302d6dfc9139c89bb9d9ad18b5e7102
William Martins <william@Note-William.house>
2018-09-11 12:19:12 -0300

    First commit

---
+++ lib/UI/klimatic.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Klimatic extends StatefulWidget {
+  @override
+  _KlimaticState createState() => _KlimaticState();
+}
+
+class _KlimaticState extends State<Klimatic> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Klimatic'),
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:klimatic/UI/klimatic.dart';
+
+void main() {
+  MaterialApp(
+    title: 'Klimatic - Tempo Agora',
+    home: Klimatic(),
+  );
+}
